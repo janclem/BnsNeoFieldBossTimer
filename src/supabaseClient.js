11177271c84c53da769fb6b65a4b303c521ab9d2
Peach <59333427+janclem@users.noreply.github.com>
2025-03-03 12:23:05 +0000

    Added env vars for superbase
    Added dotenv dependency
    
    bug fix for vercel deploy
    
    Test with output logs
    
    More changes
    
    More changes
    
    More changes
    
    More changes
    
    More changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = ''; // paste supabase url here
-const SUPABASE_ANON_KEY = ''; // paste supabase anon key here
+// console.log("ENV TEST:", process.env);
+
+const SUPABASE_URL = process.env.REACT_APP_SUPERBASE_URL; // paste supabase url here
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPERBASE_ANON_KEY; // paste supabase anon key here
+
+console.log(SUPABASE_URL)
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
